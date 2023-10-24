e8753c1ddefb7e4f790182c4cd27f6da0f8dfe45
Henry Lu <henry11241@gmail.com>
2023-10-24 16:43:45 +0800

    Create checkout steps component

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -4,15 +4,18 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import FormContainer from '../components/FormContainer'
 import { saveShippingAddress } from '../slices/cartSlice'
+import CheckoutSteps from '../components/CheckoutSteps'
 
 const ShippingScreen = () => {
   const cart = useSelector((state) => state.cart)
   const { shippingAddress } = cart
 
   const [address, setAddress] = useState(shippingAddress?.address || '')
-  const [city, setCity] = useState(shippingAddress?.city||'')
-  const [postalCode, setPostalCode] = useState(shippingAddress?.postalCode||'')
-  const [country, setCountry] = useState(shippingAddress?.country||'')
+  const [city, setCity] = useState(shippingAddress?.city || '')
+  const [postalCode, setPostalCode] = useState(
+    shippingAddress?.postalCode || ''
+  )
+  const [country, setCountry] = useState(shippingAddress?.country || '')
 
   const dispatch = useDispatch()
   const navigate = useNavigate()
@@ -25,7 +28,10 @@ const ShippingScreen = () => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
+
       <h1>Shipping</h1>
+      
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address" className="my-2">
           <Form.Label>Address</Form.Label>
